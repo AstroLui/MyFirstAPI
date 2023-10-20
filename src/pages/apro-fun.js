e76614f57f200cf2f457e2418a0d69f10e417741
Luis <142854556+AstroLui@users.noreply.github.com>
2023-10-19 21:45:19 -0400

    `Delete the file`

--- src/pages/apro-fun.js
+++
@@ -1,9 +0,0 @@
-function Apro_Fun(){
-    const url = "http://127.0.0.1:8000/apro-fun"
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('Ejercicio').innerHTML = `Resolver de manera genial este array ${data["Problema"]["Enunciado"]}`
-    })
-}
-Apro_Fun()
