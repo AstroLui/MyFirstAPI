eefd58019777b30941f44a9fa52388966ce71342
Luis <142854556+AstroLui@users.noreply.github.com>
2023-10-21 23:50:36 -0400

    feat: adding the functions for `aprop-fun.html`

--- src/main.js
+++ src/main.js
@@ -1,6 +1,62 @@
+// JS para Aproximacion de funciones
+var global_XY = 2
+function __A_Plus(){
+    const old_X = document.getElementById('X').innerHTML
+    const old_Y = document.getElementById('Y').innerHTML
+    document.getElementById('X').innerHTML = `
+    ${old_X}
+    <div id="inputs">
+        <input type="number" placeholder="1"/>
+    </div>
+    `
+    document.getElementById('Y').innerHTML = `
+    ${old_Y}
+    <div id="inputs">
+        <input type="number" placeholder="1"/>
+    </div>
+    `
+}
+function A_Add_Plus(){
+    if(global_XY < 4){
+        global_XY = global_XY + 1
+        __A_Plus()
+    }
+}
+function __A_Minus(){
+    const A_Minus_X = document.getElementById('X').querySelectorAll('#inputs') 
+    const A_Minus_Y = document.getElementById('Y').querySelectorAll('#inputs')
+    document.getElementById('X').removeChild(A_Minus_X[A_Minus_X.length-1])
+    document.getElementById('Y').removeChild(A_Minus_Y[A_Minus_Y.length-1])
+}
+function A_Delete_Minus(){
+    if(global_XY > 2){
+        __A_Minus()
+        global_XY = global_XY - 1
+    }
+}
+function Pass(){
+    const old = document.getElementById('Title-Select').innerHTML
+    const _new = document.getElementById('Nav-Select-P').innerHTML
+    document.getElementById('Title-Select').innerHTML = _new
+    document.getElementById('Nav-Select-P').innerHTML = old
+    Nav_Active()
+}
+function Nav_Active(){
+    const inputs = document.getElementById('Nav-Select-Op').querySelectorAll('div')
+    inputs.forEach(input =>{
+        if(input.classList.contains('Nav-Active')){
+            input.classList.remove('Nav-Active')
+            document.getElementById('Nav-Select').style.height = '4vh'
+        }else{
+            input.classList.add('Nav-Active')
+            document.getElementById('Nav-Select').style.height = '85px'
+            
+        }
+
+    })
+}
 // JS para Ecuaciones de una sola variable
 var global_X = 1
-let global_function
 function __Plus(sup){
     const old = document.getElementById('infuncion').innerHTML
     document.getElementById('infuncion').innerHTML = `
